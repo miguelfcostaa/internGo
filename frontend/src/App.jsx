8f7bfe76fa9de09c9994fe5ac393432331eb9b9d
Bernardo Pestana <bernardofpestana@gmail.com>
2025-07-22 14:21:37 +0100

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import HomePage from './pages/homePage';
+import HomePage from './pages/Home.jsx';
 import Login from './pages/Login';
-import SelecionarUtilizador from './pages/selecionarUtilizador';
-import ProfilePage from './pages/ProfilePage';
+import SelecionarUtilizador from './pages/SelecionarUtilizador.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <nav>
-          <Link to="/homePage">Home Page</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/selecionarUtilizador">selecionar Utilizador</Link>
-        </nav>
+        
 
         <Routes>
-          <Route path='/homePage' element={<HomePage />}></Route>
+          <Route path='/HomePage' element={<HomePage />}></Route>
           <Route path='/login' element={<Login />}></Route>
-          <Route path='/selecionarUtilizador' element={<SelecionarUtilizador />}></Route>
-          <Route path='/profile' element={<ProfilePage />}></Route>
+          <Route path='/SelecionarUtilizador' element={<SelecionarUtilizador />}></Route>
+          <Route path='/ProfilePage' element={<ProfilePage />}></Route>
         </Routes>
       </Router>
     </div>
