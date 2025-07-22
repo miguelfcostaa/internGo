d055749d9f247cf4dcffc34a427df797d510c5b8
miguelascensao1 <miguel.ascensao@acinacademy.pt>
2025-07-22 13:37:48 +0100

    adicionei a Home page, selecionar Utilizador e registo de estagiario

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/homePage';
 import Login from './pages/Login';
+import selecionarUtilizador from './pages/selecionarUtilizador';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <nav>
-          <Link to="/">Home</Link>
+          <Link to="/homePage">Home Page</Link>
           <Link to="/login">Login</Link>
+          <Link to="/selecionarUtilizador">selecionar Utilizador</Link>
         </nav>
 
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          <Route path='/homePage' element={<Home />}></Route>
           <Route path='/login' element={<Login />}></Route>
+          <Route path='/selecionarUtilizador' element={<selecionarUtilizador />}></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+function homePage() {
+    return (
+        <div>
+            <div>{/*barra superior*/}
+                <div>Logo</div>
+                <span>
+                    <form>
+                        <input type="text"></input>{/*falta acrescentar a lupa*/}
+                    </form>
+                </span>
+                <span>
+                    perfil do utilizador
+                </span>
+            </div>
+            <div>{/*barra lateral esquerda*/}
+                <div>
+                    <p>Filtro resultados:</p>
+                    <p>Informática</p>
+                </div>
+                <div>
+                    <details>
+                        <summary>Area</summary>
+                        <form>
+                            <label>
+                                <input type="checkbox"></input>
+                            </label>{" "} Informática
+                        </form>
+                    </details>
+                    <details>
+                        <summary>Regiao</summary>
+                        <form>
+                            <label>
+                                <input type="checkbox"></input>
+                            </label>{" "} Madeira
+                        </form>
+                    </details>
+                    <details>
+                        <summary>Duracao</summary>
+                        <form>
+                            <label>
+                                <input type="checkbox"></input>
+                            </label>{" "} 1 mês
+                        </form>
+                    </details>
+                    <details>
+                        <summary>Tipo de Estágio</summary>
+                        <form>
+                            <label>
+                                <input type="checkbox"></input>
+                            </label>{" "} Renumerado
+                        </form>
+                    </details>
+                </div>
+            </div>
+            <div>{/*caixa dos estagios*/}
+                <div>
+                    <div>Logo da empresa</div>
+                    <div>Nome da empresa</div>
+                </div>
+                <div>
+                    <h6>Nome do Estágio</h6>
+                    <table>
+                    <tr>
+                        <td>Total de Vagas:</td>
+                        <td>Ativas</td>
+                    </tr>
+                    <tr>
+                        <td>Area</td>
+                        <td>Início</td>
+                    </tr>   
+                    <tr>
+                        <td>Tipo de Estágio</td>
+                        <td>Duração</td>
+                    </tr>
+                    </table>
+                </div>
+                <div>
+                    <p>Lisboa,Portugal</p>
+                    <button>Candidatar-me</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default homePage;
