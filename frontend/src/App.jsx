5d3af4f3c847d0a07b985d88f324c1cf7a81c4a0
miguelfcostaa <miguelcosta220@hotmail.com>
2025-07-22 15:03:00 +0100

    apaguei

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home.jsx';
 import Login from './pages/Login';
 import SelectUser from './pages/SelectUser.jsx';
