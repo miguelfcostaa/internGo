c33c950a3b2c85a84694c729d2d2e8c423bdb896
miguelascensao1 <miguel.ascensao@acinacademy.pt>
2025-07-22 15:06:35 +0100

    porfile user

--- frontend/src/pages/ProfileUser.jsx
+++ frontend/src/pages/ProfileUser.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import logo from '../assets/logo.jpg';
 
 const ProfilePage = () => {
     return (
@@ -8,7 +9,23 @@ const ProfilePage = () => {
             <div className='container mt-5'>
                 <h1 className='text-red'>Profile Page</h1>   
             </div>
-        
+            <div>
+                <div><img src={logo} alt="Logo" /></div>
+                <span>
+                    <details>
+                        <summary>Nome do utlilizador</summary>
+                        <p>resto das informaçoes</p>
+                    </details>
+                </span>
+            </div> 
+            <div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </>
     );
 };
