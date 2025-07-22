e15c1cf2a51f0bcd54746e9cc0f110243bcab05e
miguelfcostaa <miguelcosta220@hotmail.com>
2025-07-22 11:59:15 +0100

    Adicionei a font e o Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
 
         <Routes>
           <Route path='/' element={<Home />}></Route>
-          <Route path='/login' element={<Login />}></Route>
+          <Route path='/login' element={<ProfilePage />}></Route>
         </Routes>
       </Router>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-            <h2>NavBar</h2>
+            <div>
+                <nav class="navbar bg-body-tertiary">
+                    <div class="container">
+                        <a class="navbar-brand" href="/">
+                            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24" />
+                        </a>
+                    </div>
+                </nav>
+            </div>
         );
 };
 
