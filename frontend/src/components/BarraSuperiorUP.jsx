728d1a3ec9eab452d926796ffd36cba1385c232d
maridacosta <mariana.goncalves@acinacademy.pt>
2025-07-23 15:07:57 +0100

    componente barra superior up e css do mesmo

--- frontend/src/components/BarraSuperiorUP.jsx
+++ frontend/src/components/BarraSuperiorUP.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import logo from "../assets/logo.png";
-{/* Barra superior qundo o login foi realizado*/}
+import emptyuser from "../assets/empty-user.png";
+import { Dropdown } from "react-bootstrap";
+import { FaUser } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./BarraSuperiorUP.css"; 
+
 function BarraSuperiorUP() {
-    return (
-        <div className="">
-            <div><img src={logo}/></div>
-            <div>
-                <details>
-                    <summary>Nome do utilizador</summary>
-                    <p>Logout</p>
-                </details>
-            </div>
-        </div>
-    );
+  return (
+    <div className="barra-superior d-flex justify-content-between align-items-center px-4 py-2">
+      <img src={logo} alt="Logo" className="logo" />
+
+      <Dropdown align="end">
+        <Dropdown.Toggle variant="link" className="user-toggle">
+          <FaUser className="me-2" />
+          Kanye West
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="/logout">Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
 }
+
+export default BarraSuperiorUP;
