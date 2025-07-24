52e759bce3d14421e4ccef53bb6b7126c77423d8
Bernardo Pestana <bernardofpestana@gmail.com>
2025-07-24 14:42:28 +0100

    Correção da rota de company

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
-const companyRoutes = require('./routes/routeCompany');
+const companyRoutes = require('./routes/CompanyRoutes');
 require('dotenv').config();
 
 const app = express();
