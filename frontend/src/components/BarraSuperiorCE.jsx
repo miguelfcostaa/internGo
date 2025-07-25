4d62e7d0530e96b11c4d74f15059b338721fe004
miguelascensao1 <miguel.ascensao@acinacademy.pt>
2025-07-25 15:08:04 +0100

    correção a barra superior

--- frontend/src/components/BarraSuperiorCE.jsx
+++ frontend/src/components/BarraSuperiorCE.jsx
@@ -3,17 +3,17 @@ import "../styles/BarraSuperiorCE.css";
 
 function BarraSuperiorCE() {
     return (
-        <div style={{  backgroundColor: "blue", display: "flex", flexDirection: "row"}}>
-            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", gap: "20px" }}>
-                <div style={{ color: "white"}}>Criar Estágio</div>
-                <div style={{ color: "white"}}>Gestão de Estágios</div>
-                <div style={{ color: "white"}}>Gestão de candidatos</div>
-                <div style={{ color: "white"}}>Perfil</div>
-                <div style={{ color: "white"}}>Mensagens</div>
+        <div className="Barra-azul">
+            <div className="Alinhar">
+                <div className="Componente">Criar Estágio</div>
+                <div className="Componente">Gestão de Estágios</div>
+                <div className="Componente">Gestão de candidatos</div>
+                <div className="Componente">Perfil</div>
+                <div className="Componente">Mensagens</div>
             </div>
-            <div style={{ display: "flex", flexDirection: "row", position: "absolute", right: "20px", gap: "20px"}}>
-                <div style={{ color: "white"}}>Sair</div>
-                <div style={{ color: "white"}}>Ajuda</div>
+            <div className="direita">
+                <div className="Componente">Sair</div>
+                <div className="Componente">Ajuda</div>
             </div>
         </div>
     );
