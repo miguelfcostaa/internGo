3a208752326065bda828ed9c394f39a2f1f6441c
miguelascensao1 <miguel.ascensao@acinacademy.pt>
2025-07-29 14:43:34 +0100

    feat: Refactor EstagiosAtivos table and link styles for improved UI consistency

--- frontend/src/pages/EstagiosAtivos.jsx
+++ frontend/src/pages/EstagiosAtivos.jsx
@@ -102,7 +102,7 @@ function EstagiosAtivos (){
                 </div>
                 <div style={{ maxWidth:"80%", backgroundColor:"white", marginLeft:"10%", marginRight:"10%"}}>
                     <table className={`table table-hover ${styles.border}`} style={{ maxWidth:"100%", maxHeight:"100%"}}>
-                        <thead >
+                        <thead>
                             <tr>
                                 <th>Título de Estágio</th>
                                 <td>Status</td>
@@ -125,10 +125,10 @@ function EstagiosAtivos (){
                                     <td>{element.localização}</td>
                                     <td>{element.publicado}</td>
                                     <td>
-                                        <a href="">[Ver Candidaturas]</a>
-                                        <a href="">[Editar]</a>
-                                        <a style={{color:"#007BFF"}} onClick={()=>PausarEstagio(index)}>[Pausar]</a>
-                                        <a href="">[Fechar]</a>
+                                        <a className="link">[Ver Candidaturas]</a>
+                                        <a className="link">[Editar]</a>
+                                        <a className="link" onClick={()=>PausarEstagio(index)}>[Pausar]</a>
+                                        <a className="link">[Fechar]</a>
                                     </td>
                                 </tr>
                             ))}
