950f506f383a5ca52250bca3331d1b84783f4d8e
miguelfcostaa <miguelcosta220@hotmail.com>
2025-08-12 14:41:26 +0100

    fix: Improve user name display logic in NavBar component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -174,7 +174,11 @@ const NavBar = () => {
                 onClick={() => setShowDropdown((prev) => !prev)}
               >
                 <span style={{ fontSize: "1.4rem", marginRight: 8 }}>
-                  {userInfo?.name ? `${userInfo.name.split(" ")[0]} ${userInfo.name.split(" ")[userInfo.name.split(" ").length - 1]}` : 'Loading...'}
+                  {userInfo?.name ? (
+                    userInfo.name.includes(" ") 
+                      ? `${userInfo.name.split(" ")[0]} ${userInfo.name.split(" ")[userInfo.name.split(" ").length - 1]}`
+                      : userInfo.name
+                  ) : 'Loading...'}
                 </span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
